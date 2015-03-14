30f1dde02958e9c2025e15fb66d1c6bd8354460b
holmise <johanholmlund@Johans-MacBook-Pro-5.local>
2015-03-14 13:36:25 +0100

    hemskräm

--- views/home.php
+++ views/home.php
@@ -1,5 +1,18 @@
-<div class="row">
-	<div class="col-xs">.col-xs-6</div>
-  <div class="col-xs-6">.col-xs-6</div>
-  <div class="col-xs-6">.col-xs-6</div>
+<div class="container">
+	<div class="row">
+		<div class="col-xs">
+			Hemskärm
+		</div>
+	  
+	  <div class="col-xs-6">
+	  	<h2>Registrera</h2>
+	  	<button type="submit" class="btn btn-default">Submit</button>
+	  </div>
+	  
+	  <div class="col-xs-6">
+	  	<h2>Anmäl händelse</h2>
+	  	<button type="submit" class="btn btn-default">Submit</button>
+	  </div>
+	
+	</div>
 </div>
