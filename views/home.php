e2649102a0b9af5770490ca445fc953e9e9208ee
holmise <johanholmlund@Johans-MacBook-Pro-5.local>
2015-03-14 14:01:34 +0100

    home

--- views/home.php
+++ views/home.php
@@ -1,17 +1,27 @@
 <div class="container">
-	<div class="row">
-		<div class="col-xs">
-			Hemskärm
-		</div>
+	
+	<div class="row text-center">
+		
+		<div class="col-md-8"><h3>Sverigekarta</h3></div>
+  		<div class="col-md-4 text-center">
+  			<h3>Listade händelser</h3>
+
+  		</div>
+  			
+	</div>
 	  
-	  <div class="col-xs-6">
+	 <div class="row text-center">
+
+	  <div class="col-md-6 text-center">
 	  	<h2>Registrera</h2>
-	  	<button type="submit" class="btn btn-default">Submit</button>
+	  	<p>Här kan du registrera dig för tjänsten</p>
+	  	<button type="submit" class="btn btn-default">Registrera</button>
 	  </div>
 	  
-	  <div class="col-xs-6">
-	  	<h2>Anmäl händelse</h2>
-	  	<button type="submit" class="btn btn-default">Submit</button>
+	  <div class="col-md-6 text-center ">
+	  	<h2>Anmäl iaktagelse</h2>
+	  	<p>Här kan du anmäla en iaktagelse</p>
+	  	<button type="submit" class="btn btn-danger">Anmäl</button>
 	  </div>
 	
 	</div>
