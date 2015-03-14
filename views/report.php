ec66d770eaebec388e2d199ea5dce3ea35c33f7d
emame <Emame@Emwie-6.local>
2015-03-14 14:11:12 +0100

    registersida

---
+++ views/report.php
@@ -0,0 +1,39 @@
+<h1>Anmäl iaktagelse</h1>
+<p>Skicka//Verifiera//Färdigt</p>
+<div class="row">
+  <div class="col-md-6">
+	<form class="form-horizontal">
+	  <div class="form-group">
+	    <label for="inputVart" class="col-sm-2 control-label">Vart</label>
+	    <div class="col-sm-10">
+	      <input type="Vart" class="form-control" id="inputVart" placeholder="Vart">
+	    </div>
+	  </div>
+	  <div class="form-group">
+	    <label for="inputVad" class="col-sm-2 control-label">Vad</label>
+	    <div class="col-sm-10">
+	      <input type="Vad" class="form-control" id="inputVad" placeholder="Vad">
+	    </div>
+	  </div>
+	  	  <div class="form-group">
+	    <label for="inputTel" class="col-sm-2 control-label">Telefonnummer</label>
+	    <div class="col-sm-10">
+	      <input type="Tel" class="form-control" id="inputTel" placeholder="Telefonnummer">
+	    </div>
+	  </div>
+	</form>
+  </div>
+
+
+<p>Skicka//Verifiera//Färdigt</p>
+<form class="form-inline">
+  <div class="form-group">
+    <label for="inputPassword2" class="sr-only">Password</label>
+    <input type="password" class="form-control" id="inputPassword2" placeholder="Password">
+  </div>
+  <button type="submit" class="btn btn-default">Confirm identity</button>
+</form>
+<h1>KLART</h1>
+
+<p>Skicka//Verifiera//Färdigt</p>
+<p>Kanonbra, nu är det färdigt!</p>
