52e272aa571e4cf47c3ed9bb389594d7d980adf2
holmise <johanholmlund@Johans-MacBook-Pro-5.local>
2015-03-15 00:52:57 +0100

    Footer

--- template/footer.php
+++ template/footer.php
@@ -1,9 +1,14 @@
 
 </div>
 </div>
-<footer>
-<p>Varsko - Bitch I'll warn ya<p>
-	   <div><p> Data från Lantmäteriet och MSB</p></div>
+<footer style="padding-left:40px; padding-top:60px; padding-bottom:100px;">
+
+		<a href="#" class="white" style="text-decoration:underline;">Om oss</a></br>
+		<a href="#" class="white" style="text-decoration:underline;">Kontakt</a></br>
+		<a href="#" class="white" style="text-decoration:underline;">Sitemap</a></br></br>
+	   <div class="white"><p> Data från Lantmäteriet och MSB</p></div>
+
+	   <div class="white"><p> &copy; Erik, Olivia, Emma, Johan</p></div>
 </footer>
 </body>
 </html>
