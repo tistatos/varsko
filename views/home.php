2cb38517e6bb67bf48bac64f031d9139268dc8b6
holmise <johanholmlund@Johans-MacBook-Pro-5.local>
2015-03-14 13:30:53 +0100

    hemskärmen

--- views/home.php
+++ views/home.php
@@ -1,2 +1,5 @@
-Välkommen till varsko,
-we get things done lol
+<div class="row">
+	<div class="col-xs">.col-xs-6</div>
+  <div class="col-xs-6">.col-xs-6</div>
+  <div class="col-xs-6">.col-xs-6</div>
+</div>
