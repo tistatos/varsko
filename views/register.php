cf65b33aba9601721248fa8f7eb4964eb11c66ae
emame <Emame@Emwie-6.local>
2015-03-14 17:52:54 +0100

    added active tab wizard

--- views/register.php
+++ views/register.php
@@ -5,70 +5,73 @@
 	<div class="col-md-6">
 
 
-<ul class="nav nav-tabs" role="tablist">
-    <li role="presentation" class="active">HUR</li>
-    <li role="presentation">VAD</li>
-    <li role="presentation">VERIFERIA</li>
-    <li role="presentation">KLAR</li>
-  </ul>
-  <div class="tab-content">
+		<ul class="nav nav-tabs" role="tablist">
+			<li id="hurTab"role="presentation" class="wizardactivetab">Hur</li>
+			<li id="vadTab"role="presentation">Vad</li>
+			<li id="veriTab"role="presentation">Verifiera</li>
+			<li id="klarTab"role="presentation">Klar</li>
+		</ul>
+		<div class="tab-content">
 
 			<div role="tabpanel" class="tab-pane active" id="hur">
-			<h1>HUR</h1>
-			<label class="radio-inline">
-				<input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> SMS
-			</label>
-			<label class="radio-inline">
-				<input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> SMS &amp; Mail
-			</label>
-			<label class="radio-inline">
-				<input type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3"> Mail
-			</label>
-							<a href="#vad" aria-controls="messages" role="tab" data-toggle="tab">Nästa</a>
-		</div>
-		<div role="tabpanel" class="tab-pane" id="vad">
-			<h1>VAD</h1>
-			<div class="row">
-				<div class="col-md-6">
-					<form class="form-horizontal">
-						<div class="form-group">
-							<label for="inputLan" class="col-sm-2 control-label">Län</label>
-							<div class="col-sm-10">
-								<input type="Lan" class="form-control" id="inputEmail3" placeholder="Län">
-
-							</div>
+				<h1>HUR</h1>
+				<label class="radio-inline">
+					<input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> SMS
+				</label>
+				<label class="radio-inline">
+					<input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> SMS &amp; Mail
+				</label>
+				<label class="radio-inline">
+					<input type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3"> Mail
+				</label>
+				<button href="#vad" aria-controls="messages" role="tab" data-toggle="tab" class="btn btn-default btn-next" type="submit">Nästa</button>
+			</div>
+			<div role="tabpanel" class="tab-pane" id="vad">
+				<h1>VAD</h1>
+				<div class="row">
+					<div class="col-md-6">
+						<form class="form-horizontal">
 							<div class="form-group">
-								<label for="inputKommun" class="col-sm-2 control-label">Kommun</label>
+								<label for="inputLan" class="col-sm-2 control-label">Län</label>
 								<div class="col-sm-10">
-									<input type="Kommun" class="form-control" id="inputPassword3" placeholder="Kommun">
+									<input type="Lan" class="form-control" id="inputEmail3" placeholder="Län">
+
+								</div>
+								<div class="form-group">
+									<label for="inputKommun" class="col-sm-2 control-label">Kommun</label>
+									<div class="col-sm-10">
+										<input type="Kommun" class="form-control" id="inputPassword3" placeholder="Kommun">
+									</div>
 								</div>
 							</div>
-						</div>
-					</form>
-				</div>
-				<div class="col-md-6">
-					<label class="checkbox-inline">
-						<input type="checkbox" id="inlineCheckbox1" value="1" checked> Brand
-					</label>
-					<label class="checkbox-inline">
-						<input type="checkbox" id="inlineCheckbox2" value="1" checked> Strömavbrott/El
-					</label>
-				</div>
-				<div class="col-md-6">
-					<label class="checkbox-inline">
-						<input type="checkbox" id="inlineCheckbox3" value="1"checked> Översvämning
-					</label>
-					<label class="checkbox-inline">
-						<input type="checkbox" id="inlineCheckbox4" value="1"checked> Väder/Storm
-					</label>
-					<label class="checkbox-inline">
-						<input type="checkbox" id="inlineCheckbox5" value="1"checked> Utsläpp (luft/vatten)
-					</label>
+						</form>
+					</div>
+					<div class="col-md-6">
+						<label class="checkbox-inline">
+							<input type="checkbox" id="inlineCheckbox1" value="1" checked> Brand
+						</label>
+						<label class="checkbox-inline">
+							<input type="checkbox" id="inlineCheckbox2" value="1" checked> Strömavbrott/El
+						</label>
+					</div>
+					<div class="col-md-6">
+						<label class="checkbox-inline">
+							<input type="checkbox" id="inlineCheckbox3" value="1"checked> Översvämning
+						</label>
+						<label class="checkbox-inline">
+							<input type="checkbox" id="inlineCheckbox4" value="1"checked> Väder/Storm
+						</label>
+						<label class="checkbox-inline">
+							<input type="checkbox" id="inlineCheckbox5" value="1"checked> Utsläpp (luft/vatten)
+						</label>
 
+					</div>
 				</div>
-				<a href="#klar" aria-controls="messages" role="tab" data-toggle="tab">VERIFIERA</a>
+				<button href="#hur" aria-controls="messages" role="tab" data-toggle="tab" class="btn btn-default btn-prev" type="submit">Föregående</button>
+				<button href="#veri" aria-controls="messages" role="tab" data-toggle="tab" class="btn btn-default btn-next" type="submit">Nästa</button>
+
+
 			</div>
-		</div>
 			
 			<div role="tabpanel" class="tab-pane" id="veri">
 				<h1>VERIFIKATION</h1>
@@ -76,16 +79,19 @@
 					<div class="form-group">
 						<label for="inputVerify" class="sr-only">verify</label>
 						<input type="verify" class="form-control" id="inputVerify" placeholder="Verifiera">
-					</div>
-					<button type="submit" class="btn btn-default"><a href="#klar" aria-controls="settings" role="tab" data-toggle="tab">Verifiera</a></button>
+					</div>			
 				</form>
+				<button href="#vad" aria-controls="messages" role="tab" data-toggle="tab" class="btn btn-default btn-prev" type="submit">Föregående</button>
+				<button href="#klar" aria-controls="messages" role="tab" data-toggle="tab" class="btn btn-danger btn-next" type="submit">Registrera</button>
 			</div>
 			<div role="tabpanel" class="tab-pane" id="klar">
 				<h1>KLART</h1>
 				<p>Kanonbra, nu är det färdigt!</p>
+
 			</div>
 		</div>	
 	</div>
 	<div class="col-md-1"></div>
 	<div class="col-md-1"></div>
 	<div class="col-md-1"></div>
+	<script type="text/javascript" src="js/register.js"></script>
