5285c0423a8410df6d18e6846fff0a8dfaf1a06d
tistatos <tistatos@gmail.com>
2015-03-15 08:05:50 +0100

    tweaks on register

--- views/registerNow.php
+++ views/registerNow.php
@@ -5,7 +5,6 @@ if (count($_POST)) {
      * Covert POST into a Collection object
      * for better values handling
      */
-    var_dump($_POST);
 
     $input = new \ptejada\uFlex\Collection($_POST);
 
@@ -13,7 +12,6 @@ if (count($_POST)) {
     $input->filter('Username', 'Email', 'Password', 'lan', 'kommun');
     $user->register($input);
 
-    //mail ( $input->get('Email') , "Varsko" , "gratz duude", "From: webmaster@varsko.se");
     //TODO: $userInfo->filter('');
 }
 else
@@ -26,3 +24,7 @@ else
 
 <h1>Klart!</h1>
 <p>Kanonbra, nu är det färdigt!</p>
+
+<?php
+  redirect('home', 2);
+?>
